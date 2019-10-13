97d565f3fa3b8578c327d1c07fde590ac18e5d21
Edgar <edgar.rosas@tectijuana.edu.mx>
2019-10-13 00:03:58 -0700

    Fade, menu

---
+++ Assets/Scripts/MenuScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScene : MonoBehaviour
+{
+    private CanvasGroup fadeGroup;
+    private float fadeInSpeed = 0.33f;
+
+    private void Start()
+    {
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+        fadeGroup.alpha = 1;
+
+    }
+
+    private void Update(){
+        fadeGroup.alpha = 1 - Time.timeSinceLevelLoad * fadeInSpeed;
+    }
+}
---
+++ Assets/Scripts/Preloader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Preloader : MonoBehaviour
+{
+   private CanvasGroup fadeGroup;
+   private float loadTime;
+   private float minimunLogoTime = 3.0f;
+
+    private void Start()
+    {
+        //solo habra uno
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+        //blanco
+        fadeGroup.alpha = 1;
+
+        if(Time.time < minimunLogoTime)
+            loadTime = minimunLogoTime;
+        else
+            loadTime = Time.time;
+
+    }
+
+    private void Update()
+    {
+        if(Time.time < minimunLogoTime)
+        {
+            fadeGroup.alpha = 1 - Time.time;
+        }
+        if(Time.time > minimunLogoTime && loadTime != 0)
+        {
+            fadeGroup.alpha = Time.time - loadTime;
+            if(fadeGroup.alpha >= 1){
+                SceneManager.LoadScene("Menu");
+            }
+
+        }   
+    }
+}
