6f57363f1425f70b925af9eaf8516cc220e039c8
Edgar <edgar.rosas@tectijuana.edu.mx>
2019-10-15 11:23:24 -0700

    Script RegistrarTiempo
    Para registrar el tiempo total que le tomo completar el objetivo

---
+++ Assets/Scripts/RegistrarTiempo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegistrarTiempo : MonoBehaviour
+{
+    public float tiempoObjetivo = 0f;
+    public float tiempoMinimo = 5f;
+    public float tiempoMaximo = 60f;
+
+    public static int ObtenerPuntuacion()
+    {
+        tiempoObjetivo = Time.time;
+        if(tiempoObjetivo <= tiempoMinimo){
+            //regresa la puntuacion maxima
+            return 10;
+        }
+        else if(tiempoMinimo < tiempoObjetivo && tiempoObjetivo < tiempoMaximo){
+            return 5;
+        }
+        else{
+            return 0;
+        }
+
+    }
+}
--- Assets/Scripts/TimedGaze.cs
+++ Assets/Scripts/TimedGaze.cs
@@ -7,7 +7,7 @@ public class TimedGaze : MonoBehaviour
     public GameObject textoIdentificado;
     public bool siendoObservado = false;
     //cuantos segundos debe ser observado 
-    public float duracionTiempo = 3f;
+    public float duracionTiempo = 1f;
     //cuantos segundos ha sido observado
     public float tiempoObservado = 0f;
 
@@ -20,25 +20,33 @@ public class TimedGaze : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(siendoObservado)
-        {
-            tiempoObservado += Time.deltaTime;
-            if(tiempoObservado > duracionTiempo){
-                textoIdentificado.SetActive(true);
-                //Debug.Log("Objeto identificado");
-            }
-
-        }
-        else{
-            tiempoObservado = 0f;
-            //Debug.Log("Objeto fuera de vista");
-        }
+        IdentificarObjeto();
         
     }
     //Establece si el objeto esta siendo observado o no
     //El parametro sera enviado desde el evento
     //PointerEnter = true, PointerLeave = false
-    public void EstablecerObservado(bool observado){
+    public void EstablecerObservado(bool observado)
+    {
         siendoObservado = observado;
+    }
+
+    private void IdentificarObjeto()
+    {
+        if(siendoObservado)
+        {
+            tiempoObservado += Time.deltaTime;
+            if(tiempoObservado > duracionTiempo)
+            {
+                textoIdentificado.SetActive(true);
+                //Debug.Log("Objeto identificado");
+            }
+
+        }
+        else
+        {
+            tiempoObservado = 0f;
+            //Debug.Log("Objeto fuera de vista");
+        }
     }
 }
