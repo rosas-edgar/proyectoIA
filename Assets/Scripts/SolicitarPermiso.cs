40e51486b9627a3c8ef3e582cd5771b8627fb4eb
Edgar <edgar.rosas@tectijuana.edu.mx>
2019-11-12 10:54:44 -0800

    Objetivo 2,
    falta detalles en la logica

---
+++ Assets/Scripts/SolicitarPermiso.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SolicitarPermiso : MonoBehaviour
+{
+    public string escenaSiguiente;
+    public string escenaPrevia;
+    public RegistrarTiempo registrarTiempo;
+    private SeleccionarObjeto seleccionarObjeto;
+    private string nombreObjetoSeleccionado;
+
+    private int permiso = 2; //0 para no, 1 para si, 2 para no definido aun
+    // Start is called before the first frame update
+    void Awake()
+    {
+        registrarTiempo = new RegistrarTiempo(25f, 60f);
+        seleccionarObjeto = new SeleccionarObjeto(3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (registrarTiempo.ExcedioTiempoMaximo(Time.timeSinceLevelLoad) && !seleccionarObjeto.Identificado())
+        {
+            //regresar
+            SceneManager.LoadScene(escenaPrevia);
+
+        }
+        else
+        {
+            ObjetoIdentificado();
+        }
+    }
+    //Establece si el objeto esta siendo observado o no
+    //El parametro sera enviado desde el evento
+    //PointerEnter = true, PointerLeave = false
+    public void EstablecerObservado(bool observado)
+    {
+        seleccionarObjeto.EstablecerObservado(observado);
+    }
+
+    public void EstablecerNombreObjetoSeleccionado(string nombre)
+    {
+        nombreObjetoSeleccionado = nombre;
+    }
+
+    public void ObjetoIdentificado()
+    {
+        seleccionarObjeto.ObjetoIdentificado(Time.deltaTime);
+        Debug.Log(permiso + nombreObjetoSeleccionado);
+        if (seleccionarObjeto.Identificado())
+        {
+            Debug.Log(permiso + nombreObjetoSeleccionado);
+            if(nombreObjetoSeleccionado == "Permiso" && permiso == 2) //esta solcitando permiso por primera vez
+            {
+                permiso = DecisionPermiso();
+                //mostrar algo que indique el estado del permiso
+            }
+            else if(nombreObjetoSeleccionado == "Objeto" && permiso == 1) //ya tiene permiso
+            {
+                int puntuacion = registrarTiempo.ObtenerPuntuacion(Time.timeSinceLevelLoad);
+                double tiempoObj = registrarTiempo.ObtenerTiempoObjetivo();
+                Puntuacion.puntuacionNivel = puntuacion;
+                Puntuacion.tiempoNivel += tiempoObj;
+                RegistrarPuntuacion("Nivel 2");
+                SceneManager.LoadScene(escenaSiguiente);
+            }
+            else if(nombreObjetoSeleccionado == "Objeto" && permiso == 0) //no tiene permiso
+            {
+                //tocar audio de que debe pedir permiso antes
+                SceneManager.LoadScene(escenaPrevia);
+            }
+            //temporal
+            enabled = false;
+        }
+        else
+        {
+            
+
+        }
+    }
+
+    private int DecisionPermiso()
+    {
+        int r = Random.Range(0, 100);
+        if (r > 30) return 1; //si permiso
+        else return 0; //no
+    }
+    public void RegistrarPuntuacion(string nivel)
+    {
+        SqliteHelper db = new SqliteHelper();
+        db.NuevaPuntuacion(nivel, Puntuacion.usuario.ObtenerID(), Puntuacion.tiempoNivel,
+            Puntuacion.puntuacionNivel, System.DateTime.Today.ToShortDateString());
+        Puntuacion.tiempoNivel = 0;
+        Puntuacion.puntuacionNivel = 0;
+    }
+}
