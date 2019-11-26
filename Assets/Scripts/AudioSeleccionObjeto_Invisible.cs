0476340b8a9670660c34e7e4aa3db1f43af74d6a
OsvaldoGD <reynoso.renteria.osvaldo@gmail.com>
2019-11-26 13:15:33 -0800

    Configuracion audio escena 2

---
+++ Assets/Scripts/AudioSeleccionObjeto_Invisible.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSeleccionObjeto_Invisible : MonoBehaviour
+{
+    AudioSource opcionSeleccionada;
+    public GameObject objeto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        opcionSeleccionada = GetComponent<AudioSource>();
+    }
+
+    public void playAudio()
+    {
+        if (!opcionSeleccionada.isPlaying && objeto.GetComponent<Renderer>().enabled)
+        {
+            opcionSeleccionada.PlayOneShot(opcionSeleccionada.clip);
+        }
+    }
+}
